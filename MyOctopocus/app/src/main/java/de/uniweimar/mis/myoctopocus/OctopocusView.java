b19fa16ac3c1fe238824a1df16829061c39d9b0f
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-08-22 10:59:49 +0200

    Added Octopocus Constructors

--- MyOctopocus/app/src/main/java/de/uniweimar/mis/myoctopocus/OctopocusView.java
+++ MyOctopocus/app/src/main/java/de/uniweimar/mis/myoctopocus/OctopocusView.java
@@ -1,11 +1,22 @@
 package de.uniweimar.mis.myoctopocus;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.View;
+
+import androidx.annotation.Nullable;
 
 public class OctopocusView extends View {
 
     public OctopocusView(Context context) {
         super(context);
+    }
+
+    public OctopocusView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public OctopocusView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 }
