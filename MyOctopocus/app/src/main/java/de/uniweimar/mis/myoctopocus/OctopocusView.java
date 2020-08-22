a58f9b06905187cc22b6962eecc82cef069ee529
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-08-22 10:51:34 +0200

    Added OctopocusView

---
+++ MyOctopocus/app/src/main/java/de/uniweimar/mis/myoctopocus/OctopocusView.java
@@ -0,0 +1,11 @@
+package de.uniweimar.mis.myoctopocus;
+
+import android.content.Context;
+import android.view.View;
+
+public class OctopocusView extends View {
+
+    public OctopocusView(Context context) {
+        super(context);
+    }
+}
